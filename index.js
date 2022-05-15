8bc7161101a502d157aa5d5eddd24418abad5fe6
BerkAkipek <berk.akipek.96@gmail.com>
2022-05-15 20:36:56 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+// Node Debug Mode
+// node inspect index.js 
+// cont for iterate
+// repl for examining variables at loop procedure
+// for step by step examine you have to add debugger; in loop
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+function writeCards(names, event){
+    let collector = []
+    for (let i=0; i<names.length; i++){
+        collector.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return collector;
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        n--;
+    }
+}
